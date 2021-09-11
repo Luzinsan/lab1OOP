2d97b2784f8ee6aefe7f0089e794cd2a0a98cf81
luzinsan <53607329+Luzinsan@users.noreply.github.com>
2021-09-11 22:49:11 +0700

    Update Vector3D.cpp

--- Vector3D.cpp
+++ Vector3D.cpp
@@ -1,53 +1,61 @@
 #include "Vector3D.h"
-#include <math.h>
 
 
-double Vector3D::getX() const { return m_x; };
-
-double Vector3D::getY() const { return m_y; };
-
-double Vector3D::getZ() const { return m_z; };
-
-Vector3D& Vector3D::setItems(double x, double y, double z)
+namespace luzalex 
 {
-    m_x = x;
-    m_y = y;
-    m_z = z;
-    return *this;
-}
 
-double Vector3D::module() const
-{
-    return sqrt(m_x * m_x + m_y * m_y + m_z * m_z);
-}
+    int Vector3D::m_idVector3D = 1;
 
-Vector3D& Vector3D::copyVector(Vector3D& toVec)
-{
-    toVec.m_x = m_x;
-    toVec.m_y = m_y;
-    toVec.m_z = m_z;
-    return *this;
-}
+    const double Vector3D::getX() const { return m_x; };
+    const double Vector3D::getY() const { return m_y; };
+    const double Vector3D::getZ() const { return m_z; };
 
-Vector3D& Vector3D::mltpToScal(double scal)
-{
-    m_x *= scal;
-    m_y *= scal;
-    m_z *= scal;
-    return *this;
-}
+    const Vector3D& Vector3D::setItems(double x, double y, double z)
+    {
+        m_x = x;
+        m_y = y;
+        m_z = z;
+        return *this;
+    }
 
-Vector3D& Vector3D::normVec()
-{
-    double moduleVec = module();
-    m_x /= moduleVec;
-    m_y /= moduleVec;
-    m_z /= moduleVec;
-    return *this;
-}
+    const double Vector3D::module() const
+    {
+        return sqrt(m_x * m_x + m_y * m_y + m_z * m_z);
+    }
 
-Vector3D& Vector3D::print()
-{
-    std::cout << "Vector(" << m_x << ", " << m_y << ", " << m_z << ")\n";
-    return *this;
+    const Vector3D& Vector3D::copyVector(Vector3D& toVec) const
+    {
+        toVec.m_x = m_x;
+        toVec.m_y = m_y;
+        toVec.m_z = m_z;
+        return toVec;
+    }
+    const Vector3D& Vector3D::mltpToScal(const double scal)
+    {
+        m_x *= scal;
+        m_y *= scal;
+        m_z *= scal;
+        return *this;
+    }
+    const Vector3D& Vector3D::operator*(const double scal)
+    {
+        m_x *= scal;
+        m_y *= scal;
+        m_z *= scal;
+        return *this;
+    }
+    const Vector3D& Vector3D::normVec()
+    {
+        double moduleVec = module();
+        m_x /= moduleVec;
+        m_y /= moduleVec;
+        m_z /= moduleVec;
+        return *this;
+    }
+
+    const Vector3D& Vector3D::print() const
+    {
+        std::cout << "Vector(" << m_x << ", " << m_y << ", " << m_z << ")\n";
+        return *this;
+    }
 }
