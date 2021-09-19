b97407b7adb0e7ffe86b872ff80678704987b4fe
luzinsan <53607329+Luzinsan@users.noreply.github.com>
2021-09-19 23:10:22 +0700

    Update Vector3D.h

--- Vector3D.h
+++ Vector3D.h
@@ -1,7 +1,7 @@
 #pragma once
 #include <iostream>
 
-namespace luzalex
+namespace luMath
 {
     class Vector3D
     {
@@ -11,52 +11,55 @@ namespace luzalex
         int m_id;
 
     public:
-        //Конструктор для списка uniform инициализации 1) нуль-вектора по умолчанию или 2) вектора с пользовательскими значениями
-        Vector3D(double x = 0.0, double y = 0.0, double z = 0.0) : m_x{ x }, m_y{ y }, m_z{ z }, m_id{ m_idVector3D++ }
-        {  std::cout << "Constructor - Done " << m_id << std::endl; }
+        //Конструктор для инициализации 1) нуль-вектора по умолчанию или 2) вектора с пользовательскими значениями
+        Vector3D(double x = 0.0, double y = 0.0, double z = 0.0);
 
         //Деструктор
-        ~Vector3D() { std::cout << "Destructor - Done " << m_id << std::endl; }
+        ~Vector3D();
 
         //Геттер координаты x
-        const double getX() const;
+        double getX() const;
         //Геттер координаты y
-        const double getY() const;
+        double getY() const;
         //Геттер координаты z
-        const double getZ() const;
+        double getZ() const;
 
-        //Сеттер координат вектора
-        const Vector3D& setItems(double x = 0, double y = 0, double z = 0);
+        //Сеттер координаты x
+        const Vector3D& setX(double x = 0);
+        //Сеттер координаты y
+        const Vector3D& setY(double y = 0);
+        //Сеттер координаты z
+        const Vector3D& setZ(double z = 0);
+
         //Вычисление модуля вектора
-        const double module() const;
-        //Копирование вектора - возвращает целевой вектор
-        const Vector3D& copyVector(Vector3D& toVec) const;
-        //Умножение вектора на скаляр
-        const Vector3D& mltpToScal(const double scal);
+        double module() const;
+        //Копирование вектора - возвращает вызываемый экземпляр
+        const Vector3D& copyVector(const Vector3D& fromVec);
         //Перегрузка оператора* для умножения вектора на скаляр
-        const Vector3D& operator*(const double scal);
+        friend Vector3D operator*(const Vector3D& A, const double k);
         //Нормирование вектора
         const Vector3D& normVec();
-        //Вывод в консоль трёхмерного вектора
-        const Vector3D& print() const;
+       
 
-        //Дружественная функция перегрузки оператора+ для трёхмерных векторов
-        friend const Vector3D operator+(const Vector3D& A, const Vector3D& B);
-        //Дружественная функция перегрузки оператора- для трёхмерных векторов
-        friend const Vector3D operator-(const Vector3D& A, const Vector3D& B);
-        //Дружественная функция перегрузки оператора* для векторного произведения трёхмерных векторов
-        friend const Vector3D operator*(const Vector3D& A, const Vector3D& B);
-        //Дружественная функция, возвращающая скалярное произведение друх трёхмерных векторов
-        friend const double scalarProduct(const Vector3D& A, const Vector3D& B);
-        //Дружественная функция, возвращающая косинус угла между двумя трёхмерными векторами
-        friend const double cos(const Vector3D& A, const Vector3D& B);
-        //Дружественная функция, возвращающая синус угла между двумя трёхмерными векторами
-        friend const double sin(const Vector3D& A, const Vector3D& B);
-        //Дружественная функция, возвращающая величину угла между друмя трёхмерными векторами
-        friend const double valAngle(const Vector3D& A, const Vector3D& B);
-        //Дружественная функция перегрузки оператора<< для вывода в консоль трёхмерных векторов
+        //Сложение трёхмерных векторов
+        friend Vector3D operator+(const Vector3D& A, const Vector3D& B);
+        //Разность для трёхмерных векторов
+        friend Vector3D operator-(const Vector3D& A, const Vector3D& B);
+        //Векторное произведение трёхмерных векторов
+        friend Vector3D operator*(const Vector3D& A, const Vector3D& B);
+
+        //Скалярное произведение трёхмерных векторов
+        friend double scalarProduct(const Vector3D& A, const Vector3D& B);
+        //Косинус угла между трёхмерными векторами
+        friend double cos(const Vector3D& A, const Vector3D& B);
+        //Синус угла между трёхмерными векторами
+        friend double sin(const Vector3D& A, const Vector3D& B);
+        //Значение угла между трёхмерными векторами
+        friend double valAngle(const Vector3D& A, const Vector3D& B);
+
+        //Вывод в консоль трёхмерного вектора
         friend std::ostream& operator<<(std::ostream& out, const Vector3D& vec);
-        //Дружественная функция перегрузки оператора>> для ввода через консоль трёхмерных векторов
+        //Ввод данных через консоль в инициализированный трёхмерный вектор
         friend std::istream& operator>>(std::istream& in, Vector3D& vec);
     };
 }
